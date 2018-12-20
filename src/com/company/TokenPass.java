792b7030911191bae93fdfc539f3ad0ec6fce0bb
zfeng7527 <zfeng7527@bths.edu>
2018-12-19 23:31:09 -0500

    SD

--- src/com/company/TokenPass.java
+++ src/com/company/TokenPass.java
@@ -2,14 +2,14 @@ package com.company;
 
 public class TokenPass {
     private int playerCount,currentPlayer,holder;
-
-    int[] Board = new int[10];
+    int[] Board = new int[20];
 
 public TokenPass(int playerCount){
     this.playerCount=playerCount;
 }
 
 public int [] getBoard(){
+    int[] Board = new int[playerCount];
 for(int i=0; i < playerCount-1;i++){
    Board[i] = ((int) (Math.random()*9))+1;
 }
@@ -22,7 +22,7 @@ return Board;
      Board[currentPlayer]=0;
      for(int i=holder; i>0; i--){
          player++;
-         if(player > Board.length){
+         if(player > playerCount-1){
              player=0;
          }
          Board[player]++;
